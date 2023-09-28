fbf08a671b47f15818d8a34e59af5a29aea6214d
fifovalle <fifanaufal10@gmail.com>
2023-09-28 12:35:50 +0700

    Menambahkan file latihan

---
+++ JAVA/2 OOP JAVA/5 LATIHAN/src/components/Main.java
@@ -0,0 +1,88 @@
+package components;
+
+// PEMAIN
+class Pemain {
+    String nama;
+    double darah;
+    int level;
+    // OBJEK PEMAIN
+    Senjata senjata;
+    Jirah jirah;
+
+    // CONSTRUCTOR PEMAIN
+    Pemain(String nama, double darah) {
+        this.nama = nama;
+        this.darah = darah;
+    }
+
+    // METHOD PEMAIN
+    void gunakanSenjata(Senjata senjata) {
+        this.senjata = senjata;
+    }
+
+    void gunakanJirah(Jirah jirah) {
+        this.jirah = jirah;
+    }
+
+    void tampilPemain() {
+        System.out.println("\nNama : " + this.nama + "\nDarah : " + this.darah);
+        this.senjata.tampilSenjata();
+        this.jirah.tampilJirah();
+    }
+}
+
+// SENJATA
+class Senjata {
+    double kekuatanSerangan;
+    String jenisSenjata;
+
+    // CONSTRUCTOR SENJATA
+    Senjata(String jenisSenjata, double kekuatanSerangan) {
+        this.jenisSenjata = jenisSenjata;
+        this.kekuatanSerangan = kekuatanSerangan;
+    }
+
+    void tampilSenjata() {
+        System.out.println("\nJenis Senjata : " + this.jenisSenjata + "\nKekuatan Serangan : " + this.kekuatanSerangan);
+    }
+}
+
+// JIRAH
+class Jirah {
+    double kekuatanBertahan;
+    String jenisJirah;
+
+    // CONSTRUCTOR JIRAH
+    Jirah(String jenisJirah, double kekuatanBertahan) {
+        this.jenisJirah = jenisJirah;
+        this.kekuatanBertahan = kekuatanBertahan;
+    }
+
+    void tampilJirah() {
+        System.out.println("\nJenis Jirah : " + this.jenisJirah + "\nKekuatan Bertahan : " + this.kekuatanBertahan);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // MEMBUAT OBJEK PEMAIN
+        Pemain naufal = new Pemain("Naufal", 100);
+        Pemain yusuf = new Pemain("Yusuf", 50);
+        // MEMBUAT OBJEK SENJATA
+        Senjata pisau = new Senjata("pisau", 50);
+        Senjata ketapel = new Senjata("ketapel", 100);
+        // MEMBUAT OBJEK JIRAH
+        Jirah kaos = new Jirah("kaos", 5);
+        Jirah kain = new Jirah("kain", 10);
+
+        // MEMANGGIL METHOD
+        naufal.gunakanSenjata(pisau);
+        naufal.gunakanJirah(kaos);
+        naufal.tampilPemain();
+
+        yusuf.gunakanSenjata(ketapel);
+        yusuf.gunakanJirah(kain);
+        yusuf.tampilPemain();
+    }
+}
