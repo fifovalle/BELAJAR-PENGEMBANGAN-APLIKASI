cc8acb69837386af546a5cdc87b9dd267911496f
fifovalle <fifanaufal10@gmail.com>
2023-12-11 14:03:27 +0700

    Menambahkan file operator aritmatika

---
+++ 1 JAVA/1 JAVA DASAR/7 OPERATOR ARITMATIKA/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // OPERASI ARITMATIKA
+        int variabel1 = 10;
+        int variabel2 = 20;
+        int hasil;
+
+        // PENJUMLAHAN
+        hasil = variabel1 + variabel2;
+        System.out.println(variabel1 + " + " + variabel2 + " = " + hasil);
+
+        // PENGURANGAN
+        hasil = variabel1 - variabel2;
+        System.out.println(variabel1 + " - " + variabel2 + " = " + hasil);
+
+        // PEMBAGIAN
+        hasil = variabel1 / variabel2;
+        System.out.println(variabel1 + " / " + variabel2 + " = " + hasil);
+
+        // PERKALIAN
+        hasil = variabel1 * variabel2;
+        System.out.println(variabel1 + " x " + variabel2 + " = " + hasil);
+
+        // MODULUS
+        hasil = variabel1 % variabel2;
+        System.out.println(variabel1 + " % " + variabel2 + " = " + hasil);
+    }
+}
