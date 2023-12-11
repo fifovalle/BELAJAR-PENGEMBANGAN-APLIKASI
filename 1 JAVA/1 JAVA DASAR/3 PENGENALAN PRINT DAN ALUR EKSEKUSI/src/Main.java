3f0f6569b7f66a92d5005d25b7994d17b47d9447
fifovalle <fifanaufal10@gmail.com>
2023-12-11 12:09:37 +0700

    Menambahkan file pengenalan print dan alur eksekusi

---
+++ 1 JAVA/1 JAVA DASAR/3 PENGENALAN PRINT DAN ALUR EKSEKUSI/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!2");
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!2");
+        System.out.printf("Angka = %d", 212);
+    }
+}
