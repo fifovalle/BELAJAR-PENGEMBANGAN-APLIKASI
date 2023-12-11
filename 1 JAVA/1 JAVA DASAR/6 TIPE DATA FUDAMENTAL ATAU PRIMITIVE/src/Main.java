844d3760e7041badf63098537b78a466ef60f17c
fifovalle <fifanaufal10@gmail.com>
2023-12-11 13:57:32 +0700

    Menambahkan file tipe data fudamental atau primitive

---
+++ 1 JAVA/1 JAVA DASAR/6 TIPE DATA FUDAMENTAL ATAU PRIMITIVE/src/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+    public static void main(String[] args) {
+        // TIPE DATA INTEGER
+        int i = 2147;
+        System.out.println("======INTEGER======");
+        System.out.println("INTEGER I : " + (i + 1));
+        System.out.println("NILAI MAX INTEGER : " + Integer.MAX_VALUE);
+        System.out.println("NILAI MIN INTEGER : " + Integer.MIN_VALUE);
+        System.out.println("BESAR INTEGER : " + Integer.BYTES + " BYTES");
+        System.out.println("BESAR INTEGER : " + Integer.SIZE + " BITS");
+
+        // TIPE DATA BYTE
+        byte b = 10;
+        System.out.println("======BYTE======");
+        System.out.println("BYTE B : " + b);
+        System.out.println("NILAI MAX BYTE : " + Byte.MAX_VALUE);
+        System.out.println("NILAI MIN BYTE : " + Byte.MIN_VALUE);
+        System.out.println("BESAR BYTE : " + Byte.BYTES + " BYTES");
+        System.out.println("BESAR BYTE : " + Byte.SIZE + " BITS");
+
+        // TIPE DATA SHORT
+        short s = 10;
+        System.out.println("======SHORT======");
+        System.out.println("SHORT B : " + s);
+        System.out.println("NILAI MAX SHORT : " + Short.MAX_VALUE);
+        System.out.println("NILAI MIN SHORT : " + Short.MIN_VALUE);
+        System.out.println("BESAR SHORT : " + Short.BYTES + " BYTES");
+        System.out.println("BESAR SHORT : " + Short.SIZE + " BITS");
+
+        // TIPE DATA LONG
+        long l = 10L;
+        System.out.println("======LONG======");
+        System.out.println("LONG L : " + l);
+        System.out.println("NILAI MAX LONG : " + Long.MAX_VALUE);
+        System.out.println("NILAI MIN LONG : " + Long.MIN_VALUE);
+        System.out.println("BESAR LONG : " + Long.BYTES + " BYTES");
+        System.out.println("BESAR LONG : " + Long.SIZE + " BITS");
+
+        // TIPE DATA DOUBLE
+        double d = 10.5d;
+        System.out.println("======DOUBLE======");
+        System.out.println("DOUBLE D : " + d);
+        System.out.println("NILAI MAX DOUBLE : " + Double.MAX_VALUE);
+        System.out.println("NILAI MIN DOUBLE : " + Double.MIN_VALUE);
+        System.out.println("BESAR DOUBLE : " + Double.BYTES + " BYTES");
+        System.out.println("BESAR DOUBLE : " + Double.SIZE + " BITS");
+
+        // TIPE DATA FLOAT
+        float f = 10.5f;
+        System.out.println("======FLOAT======");
+        System.out.println("FLOAT F : " + f);
+        System.out.println("NILAI MAX FLOAT : " + Float.MAX_VALUE);
+        System.out.println("NILAI MIN FLOAT : " + Float.MIN_VALUE);
+        System.out.println("BESAR FLOAT : " + Float.BYTES + " BYTES");
+        System.out.println("BESAR FLOAT : " + Float.SIZE + " BITS");
+
+        // TIPE DATA CHAR
+        char c = 'p';
+        System.out.println("======CHAR======");
+        System.out.println("CHAR C : " + c);
+        System.out.println("NILAI MAX CHAR : " + Character.MAX_VALUE);
+        System.out.println("NILAI MIN CHAR : " + Character.MIN_VALUE);
+        System.out.println("BESAR CHAR : " + Character.BYTES + " BYTES");
+        System.out.println("BESAR CHAR : " + Character.SIZE + " BITS");
+
+        // TIPE DATA BOOLEAN
+        boolean val = true;
+        System.out.println("======BOOLEAN======");
+        System.out.println("BOOLEAN VAL : " + val);
+        System.out.println("NILAI MAX BOOLEAN : " + Boolean.TRUE);
+        System.out.println("NILAI MIN BOOLEAN : " + Boolean.FALSE);
+    }
+}
