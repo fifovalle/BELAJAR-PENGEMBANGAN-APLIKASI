2bb8bfd170fc34ce665c9bec57012346632ec2e7
fifovalle <fifanaufal10@gmail.com>
2023-12-11 12:18:49 +0700

    Menambahkan file memahami string[] args

---
+++ 1 JAVA/1 JAVA DASAR/4 MEMAHAMI STRING[] ARGS/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println("Halo Kawan");
+    }
+}
