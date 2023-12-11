e6cbaf5c1e1c465384aeddef54d29965a1e34524
fifovalle <fifanaufal10@gmail.com>
2023-12-11 12:25:12 +0700

    Menambahkan file variabel, assignment dan deklarasi

---
+++ 1 JAVA/1 JAVA DASAR/5 VARIABEL, ASSIGNMENT DAN DEKLARASI/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // MEMBUAT VARIABEL
+        int x = 10;
+        System.out.println("Nilai x = " + x);
+        int y = 20;
+        System.out.println("Nilai y = " + y);
+        // DEKLARASI
+        int z;
+        z = 7;
+        System.out.println("Nilai z = " + z);
+    }
+}
