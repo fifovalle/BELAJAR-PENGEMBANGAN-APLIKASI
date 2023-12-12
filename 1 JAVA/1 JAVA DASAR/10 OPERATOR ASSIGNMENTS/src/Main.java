69eea1a1fad27dec4139889047cc37d1d9bcaedc
fifovalle <fifanaufal10@gmail.com>
2023-12-12 09:54:11 +0700

    Menambahkan file operator assignments

---
+++ 1 JAVA/1 JAVA DASAR/10 OPERATOR ASSIGNMENTS/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        // OPERATOR ASSIGNMENT
+        // ASSIGNMENT OPERATOR JUMLAH
+        int a = 1;
+        a += 10; // a = a + 10
+        System.out.println("NILAI A = " + a);
+
+        // ASSIGNMENT OPERATOR KURANG
+        int b = 20;
+        b -= 10; // b = b - 10
+        System.out.println("NILAI B = " + b);
+
+        // ASSIGNMENT OPERATOR KALI
+        int c = 20;
+        c *= 10; // c = c * 20
+        System.out.println("NILAI C = " + c);
+
+        // ASSIGNMENT OPERATOR BAGI
+        int d = 20;
+        d /= 10; // d = d / 20
+        System.out.println("NILAI D = " + d);
+
+        // ASSIGNMENT OPERATOR MODULUS
+        int e = 20;
+        e %= 10; // e = e % 20
+        System.out.println("NILAI E = " + e);
+    }
+}
