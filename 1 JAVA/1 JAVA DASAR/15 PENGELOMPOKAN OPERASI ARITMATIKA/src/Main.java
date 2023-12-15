66dedee9ee1f6dd798963d6aeda0d28483d5aa34
fifovalle <fifanaufal10@gmail.com>
2023-12-15 07:33:46 +0700

    Menambahkan file pengelompokan operasi aritmatika

---
+++ 1 JAVA/1 JAVA DASAR/15 PENGELOMPOKAN OPERASI ARITMATIKA/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // KESIMPULAN ATAU PEMBAGIAN AKAN DILAKUKAN TERLEBIH DAHULU
+        int hasil = 10 / 2 + 2 * 5;
+        System.out.println(hasil);
+
+        // PENGELOMPOKAN OPERASI DENGAN ()
+        int hasil2 = 5 * (2 * 5);
+        System.out.println(hasil2);
+    }
+}
