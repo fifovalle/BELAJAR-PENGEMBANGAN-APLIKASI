b4591cd53c7b5f19c18a0e7bf8507fea6362274a
fifovalle <fifanaufal10@gmail.com>
2023-12-16 14:11:05 +0700

    Menambahkan file if else statement

---
+++ 1 JAVA/1 JAVA DASAR/17 IF ELSE STATEMENT/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // IF STATEMENT ATAU PERCABANGAN
+        int a = 5;
+        System.out.println("NILAI A : " + a);
+
+        // PECABANGANNYA
+        if (a == 10) {
+            System.out.println("Benar");
+        } else {
+            System.out.println("Salah");
+        }
+
+        System.out.println("SELESAI");
+    }
+}
