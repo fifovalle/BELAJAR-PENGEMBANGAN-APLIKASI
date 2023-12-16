69683753e424ac0ca052e0a44ed5a9e0bcbfa394
fifovalle <fifanaufal10@gmail.com>
2023-12-16 19:02:50 +0700

    Menambahkan file nested if

---
+++ 1 JAVA/1 JAVA DASAR/19 NESTED IF/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 100;
+
+        System.out.println("AWALAN PROGRAM");
+
+        if (a == 5) {
+            if (b == 10) {
+                System.out.println("INI ADALAH PROGRAM KESIMPULAN");
+            } else {
+                System.out.println("INI ADALAH PROGRAM KESALAHAN");
+            }
+        } else {
+            System.out.println("DEFAULT CONDITION");
+        }
+        System.out.println("AKHIR PROGRAM");
+    }
+}
