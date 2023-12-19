24997ea204050a682da7f38efa758d63cdcea950
fifovalle <fifanaufal10@gmail.com>
2023-12-19 10:34:38 +0700

    Menambahkan file foor loop

---
+++ 1 JAVA/1 JAVA DASAR/26 FOOR LOOP/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int nilai;
+
+        System.out.println("AWAL PROGRAM!");
+        for (nilai = 0; nilai <= 10; nilai++) {
+            System.out.println("NILAI KE - " + nilai);
+        }
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("NILAI KE - " + i);
+        }
+        System.out.println("AKHIR PROGRAM!");
+    }
+}
