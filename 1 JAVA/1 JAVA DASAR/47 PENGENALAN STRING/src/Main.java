fe279fafad8694b5257ee306fdc2f1a842436063
fifovalle <fifanaufal10@gmail.com>
2024-01-08 11:42:15 +0700

    Menambahkan file pengenalan string

---
+++ 1 JAVA/1 JAVA DASAR/47 PENGENALAN STRING/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String kataString = "Hello World";
+        char[] kataChar = { 'h', 'a', 'p', 'p', 'y' };
+
+        System.out.println(kataString);
+        System.out.println(Arrays.toString(kataChar));
+    }
+}
